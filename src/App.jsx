8a2f2775cbf923bf712f15d1985ec8c58be3394e
Kapil Kandel <kandelkapil01@gmail.com>
2022-11-07 15:21:35 +0545

    messenger plugin updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from 'react'
 import MessengerPlugin from './Messenger'
-import { MessengerChat} from "react-messenger-chat-plugin";
 
 function App() {
   return (
     <div style={{width:'100vw',height:'100vh',background:'gray'}}>
-      <h1>messenger changes</h1>
-      <div id="fb-root"></div>
-    <div id="fb-customer-chat" class="fb-customerchat"></div>
+      <h1>MessengerChat</h1>
+    <MessengerPlugin/>
     </div>
-
   )
 }
 
--- src/Messenger.jsx
+++ src/Messenger.jsx
@@ -1,30 +1,32 @@
 import React, {useEffect, useRef} from 'react';
 
-const MessengerPlugin = (showMsngr) => {
-  useEffect(()=>{
-    const chatbox = document.getElementById('fb-customer-chat');
-    chatbox.setAttribute("100075490077422", "PAGE-ID");
-    chatbox.setAttribute("attribution", "biz_inbox");
-    window.fbAsyncInit = function() {
-      FB.init({
-        xfbml            : true,
-        version          : 'API-VERSION'
-      });
-    };
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-  },[])
-	return (
-    <>
-<div id="fb-root"></div>
-<div id="fb-customer-chat" class="fb-customerchat"></div>
-</>
 
+const MessengerPlugin = () => {
+    const MessengerRef = useRef();
+    useEffect(() => {
+    MessengerRef.current.setAttribute("page_id", "100087453825879");
+    MessengerRef.current.setAttribute("attribution", "biz_inbox");
+    window.fbAsyncInit = function () {
+        FB.init({
+            xfbml: true,
+            version: 'v15.0'
+        });
+    };
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+}, [])
+	return (
+        <>
+        <div id="fb-root"></div>
+        <div ref={MessengerRef} id="fb-customer-chat" className="fb-customerchat">
+        </div>
+        </>
 	);
 };
 
