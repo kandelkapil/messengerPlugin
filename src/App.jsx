240c47bb9c890797e7aaafd761c93c31a1749f49
Kapil Kandel <kandelkapil01@gmail.com>
2022-11-07 15:06:42 +0545

    messenger chat added

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,13 @@
 import { useState } from 'react'
 import MessengerPlugin from './Messenger'
-import { MessengerChat, } from "react-messenger-chat-plugin";
+import { MessengerChat} from "react-messenger-chat-plugin";
 
 function App() {
   return (
     <div style={{width:'100vw',height:'100vh',background:'gray'}}>
       <h1>messenger changes</h1>
-      <MessengerChat
-    pageId="109268111769502"
-    language="sv_SE"
-    themeColor={"#000000"}
-    bottomSpacing={300}
-    loggedInGreeting="loggedInGreeting"
-    loggedOutGreeting="loggedOutGreeting"
-    greetingDialogDisplay={"show"}
-    debugMode={true}
-    onMessengerShow={() => {
-      console.log("onMessengerShow");
-    }}
-    onMessengerHide={() => {
-      console.log("onMessengerHide");
-    }}
-    onMessengerDialogShow={() => {
-      console.log("onMessengerDialogShow");
-    }}
-    onMessengerDialogHide={() => {
-      console.log("onMessengerDialogHide");
-    }}
-    onMessengerMounted={() => {
-      console.log("onMessengerMounted");
-    }}
-    onMessengerLoad={() => {
-      console.log("onMessengerLoad");
-    }}
-  />
+      <div id="fb-root"></div>
+    <div id="fb-customer-chat" class="fb-customerchat"></div>
     </div>
 
   )
