857a412d46894e8536436aebe21358c5d81cad2c
Kapil Kandel <kandelkapil01@gmail.com>
2022-11-07 16:04:34 +0545

    app id added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import MessengerPlugin from './Messenger'
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
   return (
     <div style={{width:'100vw',height:'100vh',background:'gray'}}>
       <h1>MessengerChat</h1>
-    <MessengerPlugin/>
+      <MessengerCustomerChat
+        pageId="100087453825879"
+        appId="436820911941202"
+      />
     </div>
   )
 }
