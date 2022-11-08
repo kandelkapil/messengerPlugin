d1bebbec029ecf0414c809d3869e317cfab8eee4
Kapil Kandel <kandelkapil01@gmail.com>
2022-11-08 18:11:36 +0545

    messenger plugin updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
 import { useState } from 'react'
 import MessengerPlugin from './Messenger'
 import MessengerCustomerChat from 'react-messenger-customer-chat';
+import { MessengerChat,showMessenger,hideMessenger } from "react-messenger-chat-plugin";
 
 function App() {
   return (
     <div style={{width:'100vw',height:'100vh',background:'gray'}}>
       <h1>MessengerChat</h1>
-      {/* <MessengerCustomerChat
-        pageId="100087453825879"
-        appId="436820911941202"
-      /> */}
-      <MessengerPlugin/>
+      <MessengerChat
+    pageId="100087453825879"
+    language="sv_SE"
+    themeColor={"#000000"}
+    bottomSpacing={300}
+    loggedInGreeting="loggedInGreeting"
+    loggedOutGreeting="loggedOutGreeting"
+    greetingDialogDisplay={"show"}
+    debugMode={true}
+    onMessengerShow={() => {
+      console.log("onMessengerShow");
+    }}
+    onMessengerHide={() => {
+      console.log("onMessengerHide");
+    }}
+    onMessengerDialogShow={() => {
+      console.log("onMessengerDialogShow");
+    }}
+    onMessengerDialogHide={() => {
+      console.log("onMessengerDialogHide");
+    }}
+    onMessengerMounted={() => {
+      console.log("onMessengerMounted");
+    }}
+    onMessengerLoad={() => {
+      console.log("onMessengerLoad");
+    }}
+  />
+      {/* <MessengerPlugin/> */}
     </div>
   )
 }
