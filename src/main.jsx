599d2147f826433f72fee5702b7c7687d31bf01f
Kapil Kandel <kandelkapil01@gmail.com>
2022-11-13 13:42:59 +0545

    plugin added

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { MessengerChat,showMessenger,hideMessenger } from "react-messenger-chat-plugin";
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+      <MessengerChat
+    pageId="109268111769502"
+    language="sv_SE"
+    themeColor={"#3b5bdb"}
+    bottomSpacing={300}
+    loggedInGreeting="loggedInGreeting"
+    loggedOutGreeting="loggedOutGreeting"
+    greetingDialogDisplay={"show"}
+    debugMode={true}
+    onMessengerShow={() => {
+      console.log("onMessengerShow");
+    }}
+    onMessengerHide={() => {
+      console.log("onMessengerHide");
+    }}
+    onMessengerDialogShow={() => {
+      console.log("onMessengerDialogShow");
+    }}
+    onMessengerDialogHide={() => {
+      console.log("onMessengerDialogHide");
+    }}
+    onMessengerMounted={() => {
+      console.log("onMessengerMounted");
+    }}
+    onMessengerLoad={() => {
+      console.log("onMessengerLoad");
+    }}
+  /> 
+    // <App />
 )
