3d933630903fb14c50cdb4b0659e8590c57ec647
Kapil Kandel <kandelkapil01@gmail.com>
2022-12-06 23:40:07 +0545

    bot added

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { MessengerChat,showMessenger,hideMessenger } from "react-messenger-chat-plugin";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-      <MessengerChat
-    pageId="100087453825879"
-  appId="436820911941202"
+      <MessengerCustomerChat
+      pageId="100087453825879"
+      appId="436820911941202"
       /> 
 )
